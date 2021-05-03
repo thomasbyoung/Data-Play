ce8503d94aadd1e21e9ff43a084478041a2e12db
ThomasYoung <thomasbyoung12345@gmail.com>
2021-05-03 15:43:30 -0500

    configuring with datatables- need to add node/express and pass table dynamically

--- index.js
+++ index.js
@@ -6,37 +6,40 @@ fetch("data.json")
   var badArray = [];
 
   function inDaFunk(json){
+      console.log(json.length)
     for (let i = 0; i < json.length; i++ ){
         if(i < 10011){
-        console.log(i)
         var time = json[i].SnapShotDateTime;
         var time2 = json[i+1].SnapShotDateTime;
         var count = json[i].LicenseCount;
         var count2 = json[i+1].LicenseCount;
 
-        
+        //console.log(i)
         if(time === time2){
-            if(count != count2){
-                if(count > 0){
-                    goodArray.push(json[i]);
-                } 
-                if(count2 > 0){
-                    goodArray.push(json[i+1])
-                }
-            } 
-            else {
-                if(count === count2){
-                    goodArray.push(json[i])
-                }
-                else {
-                    badArray.push(json[1])
-                }
+            if(count > count2){
+                goodArray.push(json[i]);
+            } else {
+                goodArray.push(json[i+1]);
             }
+            // if(count != count2){
+            //     if(count > 0){
+            //         goodArray.push(json[i]);
+            //     } 
+            //     if(count2 > 0){
+            //         goodArray.push(json[i+1])
+            //     }
+            // } 
+            // else {
+            //     if(count === count2){
+            //         goodArray.push(json[i])
+            //     }
+            //     else {
+            //         badArray.push(json[1])
+            //     }
+            // }
         } 
     }
     }
-    console.log(goodArray[0]);
-    console.log(badArray);
     buildOutput()
 }
 
@@ -44,7 +47,6 @@ function buildOutput(){
     for(let i=0;i<goodArray.length;i++){
 
         var row = document.createElement("tr");
-       // para.appendChild(node);
 
         var para = document.createElement("td");
         var node = document.createTextNode(goodArray[i].SnapShotDateTime);
@@ -59,7 +61,11 @@ function buildOutput(){
         row.appendChild(para);
         row.appendChild(para2);
     }
+   // $('#example').DataTable();
 }
 
 
-  document.getElementById("demo").innerHTML = goodArray[0];
+//   $(document).ready(function() {
+//     $('#example').DataTable();
+// } );
+
